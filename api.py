b576e995a2d190c0225f2f2da41d325dd04c7228
Your Name <you@example.com>
2025-01-11 22:37:56 +0530

    s1

--- api.py
+++ api.py
@@ -1,12 +1,22 @@
 import requests
 import json
+import boto3
 
 api_url = 'https://jsonplaceholder.typicode.com/posts'
+
+response = requests.get(api_url)
+attributes = dir(response)
+print(attributes)
+
+ec2_client = boto3.client('ec2')
+attributes_ec2 = dir(ec2_client.run_instances)
+print(attributes_ec2)
+print(ec2_client.__str__)
 
 def function_api():
     try:
         response = requests.get(api_url)
-
+        print(response)
         if response.status_code == 200:
             print(f"Status code: ", response.status_code)
             posts = response.json()
